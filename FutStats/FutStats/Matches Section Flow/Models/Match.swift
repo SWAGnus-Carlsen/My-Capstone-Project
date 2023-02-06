56ab57fb85fb55489038845cc9a5967813279cd7
SWAGnus-Carlsen <92265364+SWAGnus-Carlsen@users.noreply.github.com>
2023-02-07 01:17:56 +0300

    smth new added))

---
+++ FutStats/FutStats/Matches Section Flow/Models/Match.swift
@@ -0,0 +1,14 @@
+//
+//  Match.swift
+//  FutStats
+//
+//  Created by Vitaliy Halai on 6.02.23.
+//
+
+import UIKit
+
+struct Match: Codable {
+    let hostTeam: Team?
+    let arrivedTeam: Team?
+    let score: Score?
+}
---
+++ FutStats/FutStats/Matches Section Flow/Models/Team.swift
@@ -0,0 +1,14 @@
+//
+//  Team.swift
+//  FutStats
+//
+//  Created by Vitaliy Halai on 6.02.23.
+//
+
+import UIKit
+ 
+struct Team: Codable {
+    let league: String?
+    let name: String?
+    let numberOfPLayers: Int?
+}
